d8a808fd1c322b9e45f2978264ecc3ce6a317e80
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-30 09:42:09 -0400

    updating code
    total is appearing in your order with remove button and price

--- script.js
+++ script.js
@@ -42,20 +42,65 @@ function renderMenu() {
         button.addEventListener('click', addToOrder);
     });
 }
+// Define a global variable to keep track of the total price
+let totalPrice = 0;
 
-// Function to handle the click event on the "add-to-order" button
 function addToOrder(event) {
-    const menuItem = event.target.parentNode.querySelector('.menu-details').innerHTML;
+    const menuItem = event.target.parentNode.querySelector('.menu-details');
+    const itemName = menuItem.querySelector('.name').textContent;
+    const itemPrice = parseFloat(menuItem.querySelector('.price').textContent.replace('$', ''));
 
     // Example code to update and display the order list
     const orderList = document.getElementById('order-list');
     const orderItem = document.createElement('li');
-    orderItem.innerHTML = menuItem;
+    orderItem.classList.add('order-item');
+
+    const itemNameElement = document.createElement('span');
+    itemNameElement.classList.add('item-name');
+    itemNameElement.textContent = itemName;
+
+    const itemPriceElement = document.createElement('span');
+    itemPriceElement.classList.add('item-price');
+    itemPriceElement.textContent = `$${itemPrice.toFixed(2)}`;
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove-button');
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', removeItem);
+
+    orderItem.appendChild(itemNameElement);
+    orderItem.appendChild(removeButton);
+    orderItem.appendChild(itemPriceElement);
+
     orderList.appendChild(orderItem);
+
+    // Update the total price
+    totalPrice += itemPrice;
+    const totalPriceElement = document.getElementById('total-price-amount');
+    totalPriceElement.textContent = totalPrice.toFixed(2);
 
     // Show the "Your Order" section if it was previously hidden
     const yourOrderSection = document.getElementById('your-order');
     yourOrderSection.style.display = 'block';
+}
+
+function removeItem(event) {
+    const orderItem = event.target.parentNode;
+    const itemPrice = parseFloat(orderItem.querySelector('.item-price').textContent.replace('$', ''));
+    
+    // Update the total price
+    totalPrice -= itemPrice;
+    const totalPriceElement = document.getElementById('total-price-amount');
+    totalPriceElement.textContent = totalPrice.toFixed(2);
+
+    const orderList = orderItem.parentNode;
+    orderList.removeChild(orderItem);
+
+    // Hide the "Your Order" section if there are no items left
+    if (orderList.children.length === 0) {
+        const yourOrderSection = document.getElementById('your-order');
+        yourOrderSection.style.display = 'none';
+    }
 }
 
 renderMenu();
