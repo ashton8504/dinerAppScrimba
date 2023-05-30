57baee9d66709f512a5115613a51c21b79e9379f
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-30 11:46:58 -0400

    updated code
    Got Modal working and will now work on styling it

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const menu = document.getElementById("menu");
 const completeOrderBtn = document.getElementById('complete-order-btn');
 const modal = document.getElementById('modal');
 const closeBtn = document.querySelector('.close');
-
 completeOrderBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 
+
+// Modal Section
 function openModal() {
   modal.style.display = 'block';
 }
@@ -16,6 +17,7 @@ function closeModal() {
   modal.style.display = 'none';
 }
 
+// This is pulling menu info from our data file and showing menu for app
 function getMenuItems() {
     let menuFeed = '';
     menuArray.forEach(function (menuItem) {
@@ -46,6 +48,7 @@ function getMenuItems() {
     return menuFeed;
 }
 
+// This is rendering the menu 
 function renderMenu() {
     const menuFeed = getMenuItems();
     menu.innerHTML = menuFeed;
@@ -56,9 +59,12 @@ function renderMenu() {
         button.addEventListener('click', addToOrder);
     });
 }
+
+
 // Define a global variable to keep track of the total price
 let totalPrice = 0;
 
+// Section allows us to add to our order
 function addToOrder(event) {
     const menuItem = event.target.parentNode.querySelector('.menu-details');
     const itemName = menuItem.querySelector('.name').textContent;
@@ -98,6 +104,7 @@ function addToOrder(event) {
     yourOrderSection.style.display = 'block';
 }
 
+// Remove an item from our order
 function removeItem(event) {
     const orderItem = event.target.parentNode;
     const itemPrice = parseFloat(orderItem.querySelector('.item-price').textContent.replace('$', ''));
@@ -116,5 +123,43 @@ function removeItem(event) {
         yourOrderSection.style.display = 'none';
     }
 }
+
+// Modal styles for thank you message after payment is submitted 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("order-form");
+    const modal = document.getElementById("modal");
+    const modalContent = document.querySelector(".modal-content");
+    const thankYouModal = document.getElementById("thank-you-modal");
+    const thankYouMessage = document.getElementById("thank-you-message");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const nameInput = document.getElementById("name");
+      const name = nameInput.value;
+  
+      modal.style.display = "none";
+      thankYouMessage.textContent = "Thank you for your order, " + name + "!";
+      thankYouModal.style.display = "block";
+  
+      form.reset();
+  
+      setTimeout(function() {
+        thankYouModal.style.display = "none";
+        location.reload();
+      }, 3000);
+    });
+  
+    const closeBtns = document.querySelectorAll(".close");
+    closeBtns.forEach(function(btn) {
+      btn.addEventListener("click", function() {
+        modal.style.display = "none";
+        thankYouModal.style.display = "none";
+      });
+    });
+  });
+  
+
+
 
 renderMenu();
